dbfcce190d83dd271ec48163eaef12559094c1b2
unknown <vshnvdfjk@gmail.com>
2023-06-27 22:33:48 +0300

    first commit

---
+++ SplashScreen_imports.dart
@@ -0,0 +1,3 @@
+import 'package:flutter/material.dart';
+part 'SplashScreen.dart';
+part './widgets/SplashScreen_body.dart';
---
+++ widgets/SplashScreen_body.dart
@@ -0,0 +1,11 @@
+part of '../SplashScreen_imports.dart';
+
+
+class SplashScreenBody extends StatelessWidget {
+const SplashScreenBody({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Container();
+  }
+}
